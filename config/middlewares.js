304a54f876ffff9158863e75c3941537862a0714
a <a@MacBook-Air-de-a.local>
2024-04-26 18:44:17 +0900

    viernes26cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,35 @@
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
+
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
@@ -9,4 +37,6 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  
 ];
+
